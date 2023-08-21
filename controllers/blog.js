6bdca07794fbc523f3c1923c32e6f79ff4e109c9
Thomas Aspbury <tomaspbury@icloud.com>
2023-08-21 11:27:30 +0100

    Complete exercise 13.13

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Op } = require('sequelize');
 const { User, Blog } = require('../models');
 const extractToken = require('../utils/extract-token');
 
@@ -15,6 +16,14 @@ const blogFinder = async (req, res, next) => {
 };
 
 router.get('/', async (req, res, next) => {
+  const where = {};
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`,
+    };
+  }
+
   try {
     const blogs = await Blog.findAll({
       attributes: {
@@ -24,6 +33,7 @@ router.get('/', async (req, res, next) => {
         model: User,
         attributes: ['name'],
       },
+      where,
     });
 
     res.json(blogs);
