a13b71935cc8250bd2fd491812a0cae3fb1522a5
Thomas Aspbury <tomaspbury@icloud.com>
2023-08-21 11:48:11 +0100

    Complete exercise 13.14

--- controllers/blog.js
+++ controllers/blog.js
@@ -18,10 +18,25 @@ const blogFinder = async (req, res, next) => {
 router.get('/', async (req, res, next) => {
   const where = {};
 
+  // if (req.query.search) {
+  //   where.title = {
+  //     [Op.iLike]: `%${req.query.search}%`,
+  //   };
+  // }
+
   if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`,
-    };
+    where[Op.or] = [
+      {
+        title: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+      },
+      {
+        author: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+      },
+    ];
   }
 
   try {
