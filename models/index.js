31b825fed19cf0ca4dfb8be4e733992705b1c9e0
Thomas Aspbury <tomaspbury@icloud.com>
2023-08-22 11:43:42 +0100

    Complete exercise 13.17

--- models/index.js
+++ models/index.js
@@ -4,9 +4,6 @@ const User = require('./user');
 User.hasMany(Blog, { foreignKey: 'userId' });
 Blog.belongsTo(User, { foreignKey: 'userId' });
 
-Blog.sync({ alter: true });
-User.sync({ alter: true });
-
 module.exports = {
   Blog,
   User,
