66e8cdca6306ad794b909f3b503d5e7a3c4c91a0
Thomas Aspbury <tomaspbury@icloud.com>
2023-08-23 19:22:47 +0100

    Complete exercise 13.22

--- controllers/reading-list.js
+++ controllers/reading-list.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ReadingList } = require('../models');
+const extractToken = require('../utils/extract-token');
 
 const router = express.Router();
 
@@ -7,6 +8,29 @@ router.post('/', async (req, res, next) => {
   try {
     const readingList = await ReadingList.create({ ...req.body });
     res.status(201).json(readingList);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', extractToken, async (req, res, next) => {
+  try {
+    const readingList = await ReadingList.findByPk(req.params.id);
+
+    if (!readingList) {
+      return res.status(404).end();
+    }
+
+    if (readingList.userId !== req.tokenPayload.id) {
+      return res
+        .status(401)
+        .json({ error: 'Only owner of this list may mark as read' });
+    }
+
+    readingList.isRead = req.body.isRead;
+    await readingList.save();
+
+    res.status(200).json(readingList);
   } catch (err) {
     next(err);
   }
